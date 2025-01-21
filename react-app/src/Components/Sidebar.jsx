3a1f6d2b0af5c105df55d82e4d755a1eb1ca4f44
Uttkarsh <uttkarshsaini10@gmail.com>
2025-01-21 13:48:55 +0530

    Getting Started with my portfolio website

---
+++ react-app/src/Components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+      <div>
+          <h4>Uttkarsh</h4>
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default Sidebar
